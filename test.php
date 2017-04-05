2d85822f6151ea5151a787606a64ee8a35111b80
evenkill <vincent.georges1994@gmail.com>
2017-04-05 12:12:04 +0200

    Creation premier fichier - test.php - ajout de date

---
+++ test.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mon premier code php (date)</title>
+</head>
+<body>
+<p>Aujourdhui nous les sommmes le : <?php echo date('d/m/Y h:i:s'); ?>.</p>
+</body>
+</html>
