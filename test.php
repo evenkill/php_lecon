5d93beef3d742d0727f11b0670bcf2f480dbc671
evenkill <vincent.georges1994@gmail.com>
2017-04-05 14:15:17 +0200

    Ajout de - echo <strong>

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
-<DOCTYPE html>
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Mon premier code php (date)</title>
 </head>
 <body>
-<p>Aujourdhui nous les sommmes le : <?php echo date('d/m/Y h:i:s'); ?>.</p>
+<p>Aujourdhui nous les sommmes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+<p><?php echo '<strong>Bonjour !</strong>' ?></p>
 </body>
 </html>
